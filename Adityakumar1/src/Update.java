2cced3e18f6158ca917a5888f619333569614b12
sdac <sdac@sdac-PC>
2023-06-14 15:33:40 +0530

    1.0.1

--- Adityakumar1/src/Update.java
+++ Adityakumar1/src/Update.java
@@ -1,8 +1,8 @@
 
 public class Update {
-public static void main(String[] args) {
-	System.out.println("Update 1.0");
-	System.out.println("Update");
-System.out.println();
-}
+	public static void main(String[] args) {
+		System.out.println("Update 1.0");
+		System.out.println("Update");
+		System.out.println("1.0.1");
+	}
 }
